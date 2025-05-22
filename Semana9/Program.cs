7fc029e5e0e21beb33dd60658940270f019c2254
day130725 <N00439638@upn.pe>
2025-05-22 12:02:42 -0500

    Agregar archivos de proyecto.

---
+++ Semana9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MIBIBLIOTECA;
+
+namespace Semana9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 day = new Class1();
+            //day.sumar();
+            //Console.ReadKey();
+
+            int resultado = 0;
+
+            resultado = 10;
+            day.Resta(ref resultado);
+            Console.WriteLine("la resta es: "+ resultado);
+            
+            Calculadora c = new Calculadora();
+            int r2;
+            c.multiplicar(out r2);
+            Console.WriteLine("la Multiplicacion es: " + r2);
+
+            Console.ReadKey();
+        }
+    }
+}
